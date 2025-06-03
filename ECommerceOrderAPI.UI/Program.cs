4b077a659bdde3a55ede17415067020594706b3d
Erqes <102285533+Erqes@users.noreply.github.com>
2025-06-03 17:38:12 +0200

    swagger

--- ECommerceOrderAPI.UI/Program.cs
+++ ECommerceOrderAPI.UI/Program.cs
@@ -17,11 +17,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerceOrderAPI V1");
+});
 
 app.UseHttpsRedirection();
 app.UseRouting();
